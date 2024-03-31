d0ec236e71be06b35463075bc03a755de2024f02
Akshaya HP <akshayhulkuli@gmail.com>
2024-03-31 17:46:11 +0530

    initialized code

---
+++ src/main/java/com/akshay/kafkastreams/kafkastreamspringboot/KafkaStreamSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.kafkastreams.kafkastreamspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/kafkastreams/kafkastreamspringboot/KafkaStreamSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.kafkastreams.kafkastreamspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
